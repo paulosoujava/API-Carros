a7c5b93c78358f70d7b509f309eddea4802cce9d
Paulo <paulosoujava@gmail.com>
2020-01-29 16:31:37 -0300

    UPLOAD

---
+++ src/main/java/api/upload/UploadOutput.java
@@ -0,0 +1,13 @@
+package api.upload;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UploadOutput {
+    private String url;
+}
